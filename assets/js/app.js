f041e124bcbc1ad19544f0e87b6c1a1a5ba07231
sonng147202 <sonnguyen20140720@gmail.com>
2022-11-05 16:51:11 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,34 +26,47 @@ $( document ).ready(function() {
       },
     ]
   });
-  // Responsive
-  $(window).resize(function() {
-    if (window.matchMedia('(min-width: 992px)').matches) {
-      $("#banner .content .right img").attr("src","./assets/img/banner.png");
-    }
-    // 992px
-    if (window.matchMedia('(max-width: 992px)').matches) {
-      
-    }
-    // 768px
-    if (window.matchMedia('(max-width: 768px)').matches) {
-      console.log($("#banner .content .right"));
-      $("#banner .content .right img").attr("src","./assets/img/banner-mobi.png");
-      // feature
-      var tabStandard = $("#tab-standard");
-      var tabRofessional= $("#tab-rofessional");
-      var tabui= $(".tabui");
-      var tabhindin= $(".tabhindin");
 
-      tabStandard.click(function () {
-        tabui.css("display", "block");
-        tabhindin.css("display", "none");
-      });
-      tabRofessional.click(function () {
-        tabhindin.css("display", "block");
-        tabui.css("display", "none");
-      });
-    }
-  })
+  var btnAccordion = $(".accordion .accordion-button");
+  var arrowAccordion = $(".accordion .accordion-button .accordion-arrow");
+
+  for (let i = 0; i < arrowAccordion.length; i++) {
+    const btn = btnAccordion[i];
+    const arrow = arrowAccordion[i];
+
+    btn.click(() => {
+      arrow.toggleClass("up");
+    })
+  }
+  
   AOS.init();
 });
+// Responsive
+$(window).resize(function() {
+  if (window.matchMedia('(min-width: 992px)').matches) {
+    $("#banner .content .right img").attr("src","./assets/img/banner.png");
+  }
+  // 992px
+  if (window.matchMedia('(max-width: 992px)').matches) {
+    
+  }
+  // 768px
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    console.log($("#banner .content .right"));
+    $("#banner .content .right img").attr("src","./assets/img/banner-mobi.png");
+    // feature
+    var tabStandard = $("#tab-standard");
+    var tabRofessional= $("#tab-rofessional");
+    var tabui= $(".tabui");
+    var tabhindin= $(".tabhindin");
+
+    tabStandard.click(function () {
+      tabui.css("display", "block");
+      tabhindin.css("display", "none");
+    });
+    tabRofessional.click(function () {
+      tabhindin.css("display", "block");
+      tabui.css("display", "none");
+    });
+  }
+})
